d056d512de6e74b22dc198dfb1b4e9e6453c00f2
Rafael Decker <rafaeldecker@gmail.com>
2019-06-02 03:06:31 -0300

    added tests for fleet type mapper

---
+++ app/src/test/kotlin/com/mytaxy/test/infra/api/mappers/FleetTypeMapperTest.kt
@@ -0,0 +1,44 @@
+package com.mytaxy.test.infra.api.mappers
+
+import com.mytaxy.test.entities.FleetType
+import com.mytaxy.test.util.Mapper
+import org.junit.Assert
+import org.junit.Before
+import org.junit.Test
+
+/**
+ * Created by Rafael Decker on 2019-06-02.
+ */
+
+class FleetTypeMapperTest {
+
+    private lateinit var mapper: Mapper<String, FleetType>
+
+    @Before
+    fun setup() {
+        mapper = FleetTypeMapper()
+    }
+
+    @Test
+    fun testMappingTaxi() {
+        var type = mapper.map("Taxi")
+        Assert.assertEquals(type, FleetType.TAXI)
+        type = mapper.map("taxi")
+        Assert.assertEquals(type, FleetType.TAXI)
+    }
+
+    @Test
+    fun testMappingPoollig() {
+        var type = mapper.map("Pooling")
+        Assert.assertEquals(type, FleetType.POOLING)
+        type = mapper.map("pooling")
+        Assert.assertEquals(type, FleetType.POOLING)
+    }
+
+    @Test
+    fun testMappingUnknown() {
+        val type = mapper.map("taxxi")
+        Assert.assertEquals(type, FleetType.UNKNOWN)
+    }
+
+}
